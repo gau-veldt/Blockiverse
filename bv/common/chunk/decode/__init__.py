d7a2f0262aa578c397fa956a58c95d080968d5ba
gau-veldt <gau_veldt@hotmail.com>
2014-10-01 14:48:22 -0700

    more work on decode stuff...
    more work on decode stuff...

--- bv/common/chunk/decode/__init__.py
+++ bv/common/chunk/decode/__init__.py
@@ -8,39 +8,82 @@ def decompress(decoder):
     Decorator to factor out chunk
     decompression logic from decoder routines
     """
-    def wrapper(blob,origin,viewpoint):
-        return decoder(zlib.decompress(blob),origin,viewpoint)
+    def wrapper(self,blob,origin):
+        return decoder(self,zlib.decompress(blob),origin)
     return wrapper
 
 from panda3d.core import Geom,GeomVertexFormat
 from panda3d.core import GeomTriangles,GeomNode
 
-@decompress
-def Chunk2Geom(chunk,origin,viewpoint):
+class Chunk2GeomDecoder:
     """
-    Decodes chunk into Panda3D geometry format
-    @param
-        chunk: encoded chunk
-        origin: chunk destination in world coordinates
-        viewpoint: viewer reference point in world coordinates
-                    (used for LOS culling of non-visible blocks
-                    to reduce triangle load)
-    @return
-        A panda3D Node object translated appropriately to place
-        the decoded chunk correctly within the world.
+        Chunk decoder that decodes chunks to Panda3D Geoms/Nodes
     """
-    # determine where chunk should be placed in world space
-    orgX=origin[0]
-    orgY=origin[1]
-    orgZ=origin[2]
-    # determine chunk's coordinate
-    chunkX=orgX/16
-    chnukY=orgY/16
-    # generate name tags for the various parts
-    chunkId="%s_%s" % (chunkX,chunkY)
-    vtxName="vtx_%s" % chunkId
-    nodeName="node_%s" % chunkId
-    # TODO: read chunk data and generate triangles
+    def __init__(self):
+        self.viewPoint=(0,0,0)
+        self.cubeVerts=[
+            # 14 vertices per cube mapped
+            # so that UV coords utilize
+            # the typical cube unwrap:
+            #
+            #   ####  <-- square texture
+            #   ##U#
+            #   BLFR
+            #   ##D#    #=unused
+            #
+            # This form uses 14 vertices per cube since
+            # the extra (worldspace overlapped) vertices
+            # map different U,V coordinates at the same
+            # worldspace coordinates, depending on face
+            #   
+            #   x,   y,   z,   u,   v
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00),
+            (0.00,0.00,0.00,0.00,0.00) 
+        ]
+        # Generate vertices for 1x1x256 'sliver'
+        # starting at bottom (z=0), towards top (z=255)
+
+    def setViewpoint(x,y,z):
+        self.viewPoint=(x,y,z)
+
+    @decompress
+    def Chunk2Geom(self,chunk,origin):
+        """
+        Decodes chunk into Panda3D geometry format
+        @param
+            chunk: encoded chunk
+            origin: chunk destination in world coordinates
+            viewpoint: viewer reference point in world coordinates
+                        (used for LOS culling of non-visible blocks
+                        to reduce triangle load)
+        @return
+            A panda3D Node object translated appropriately to place
+            the decoded chunk correctly within the world.
+        """
+        # determine where chunk should be placed in world space
+        orgX=origin[0]
+        orgY=origin[1]
+        orgZ=origin[2]
+        # determine chunk's coordinate
+        chunkX=orgX/16
+        chnukY=orgY/16
+        # generate name tags for the various parts
+        chunkId="%s_%s" % (chunkX,chunkY)
+        vtxName="vtx_%s" % chunkId
+        nodeName="node_%s" % chunkId
+        # TODO: read chunk data and generate triangles
 
 if __name__ == '__main__':
     pass
